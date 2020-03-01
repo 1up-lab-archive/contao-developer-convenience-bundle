cd8c94aee5b4bb99238d0b7e72245a4704925fb3
David Greminger <david.greminger@1up.io>
2020-03-01 23:39:55 +0100

    Removed leftovers

--- DeveloperConvenienceBundle.php
+++ DeveloperConvenienceBundle.php
@@ -4,14 +4,8 @@ declare(strict_types=1);
 
 namespace Oneup\DeveloperConvenienceBundle;
 
-use Oneup\DeveloperConvenienceBundle\DependencyInjection\Compiler\AddContaoDatabaseUpdatePass;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class DeveloperConvenienceBundle extends Bundle
 {
-    public function build(ContainerBuilder $container): void
-    {
-        $container->addCompilerPass(new AddContaoDatabaseUpdatePass());
-    }
 }
